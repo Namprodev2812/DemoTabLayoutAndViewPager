d88a6139d7466d1b4ad521601b81f1c45d0ba0a5
Namprodev2812 <mailakudoshinichi17@gmail.com>
2019-04-11 22:02:55 +0700

    init_project

---
+++ app/src/main/java/com/asterisk/nam/demotablayoutviewpager/MainActivity.java
@@ -0,0 +1,14 @@
+package com.asterisk.nam.demotablayoutviewpager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
